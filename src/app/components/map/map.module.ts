228bf2c3f55bf452854cf4433e40571b62298dad
novakand <a79264799999@gmail.com>
2022-02-20 23:48:41 +0300

    fix map module

--- src/app/components/map/map.module.ts
+++ src/app/components/map/map.module.ts
@@ -1,7 +1,11 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // components
 import { LegendComponent } from './components/legend/legend.component';
@@ -12,6 +16,13 @@ import { MapComponent } from './components/map/map.component';
         CommonModule,
         GoogleMapsModule,
         DragDropModule,
+        CommonModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
     ],
     declarations: [
         LegendComponent,
